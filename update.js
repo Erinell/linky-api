a7604ccba63bb179c08e45af0ceaab274f28bd7a
Erinell <ceddebois@gmail.com>
2022-12-24 18:27:38 +0100

    update

--- update.js
+++ update.js
@@ -1,5 +1,28 @@
 const AutoGitUpdate = require('auto-git-update');
 
+let main = function() {
+    let self = this;
+    const config = {
+        repository: 'https://github.com/Erinell/linky-api',
+        fromReleases: false,
+        tempLocation: '../tmp/',
+        ignoreFiles: ['config.js'],
+        executeOnComplete: 'npm start',
+        exitOnComplete: true
+    };
+    let updater = new AutoGitUpdate(config);
+    self.start = function() {
+
+    }
+    self.checkUpdate = function (){
+        const current = updater.compareVersions();
+        if(current.upToDate) return console.log(`API à jour (${currentVersion})`);
+    
+        console.log(`La mise à jour ${remoteVersion} est disponible (version actuelle : ${currentVersion})`);
+        updater.autoUpdate();
+    }
+}
+
 module.exports = {
     initAutoUpdate: function () {
         const config = {
